322e5ec6d198e6ce8fe204fa09ba74e1ba975b94
aldalee <hadoop_2020@163.com>
2023-02-08 19:56:26 +0800

    Scala批数据处理词频统计

--- learn-flink/src/main/scala/com/msb/wc/BatchWordCount.scala
+++ learn-flink/src/main/scala/com/msb/wc/BatchWordCount.scala
@@ -2,11 +2,18 @@ package com.msb.wc
 
 import org.apache.flink.api.scala.ExecutionEnvironment
 
+/**
+ * Scala: Flink DataSet 批处理WordCount
+ */
 object BatchWordCount {
     def main(args: Array[String]): Unit = {
+        //准备Scala对应的Flink环境
         val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+        //使用Scala API时需要隐式转换来推断函数操作后的类型
         import org.apache.flink.api.scala._
+        //读取数据文件
         val ds: DataSet[String] = env.readTextFile("./data/words.txt")
+        //进行WordCount统计并打印
         ds.flatMap(_.split(" "))
           .map((_, 1))
           .groupBy(0)
