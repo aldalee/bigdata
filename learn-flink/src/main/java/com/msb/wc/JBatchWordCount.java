7088ba263b1054d6ed10ee1c6f7dadec2550b0a5
aldalee <hadoop_2020@163.com>
2023-02-08 19:49:25 +0800

    Initial commit

---
+++ learn-flink/src/main/java/com/msb/wc/JBatchWordCount.java
@@ -0,0 +1,8 @@
+package com.msb.wc;
+
+
+public class JBatchWordCount {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ learn-flink/src/main/scala/com/msb/wc/BatchWordCount.scala
@@ -0,0 +1,16 @@
+package com.msb.wc
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+object BatchWordCount {
+    def main(args: Array[String]): Unit = {
+        val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+        import org.apache.flink.api.scala._
+        val ds: DataSet[String] = env.readTextFile("./data/words.txt")
+        ds.flatMap(_.split(" "))
+          .map((_, 1))
+          .groupBy(0)
+          .sum(1)
+          .print()
+    }
+}
